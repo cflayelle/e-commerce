acc1ccc4b0f6761f1a5ec4a2e196cb2925882247
Flayelle Camille <camille@deCamille.local>
2022-03-16 16:55:20 +0100

    products & affichage des produits & insert base de donnée

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="app_product")
+     * @Route("/products", name="product")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products
         ]);
     }
 }
