1a9dc60a5445aa9f7561043086a6355f78417a34
Mon Macbook <wendykrwn@gmail.com>
2022-03-18 00:05:07 +0100

    ajout produit

--- src/Controller/AdminProductController.php
+++ src/Controller/AdminProductController.php
@@ -2,21 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class AdminProductController extends AbstractController
 {
-    #[Route('/admin/product', name: 'app_admin_product')]
+    #[Route('/admin/product', name: 'admin_product')]
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
 
         return $this->render('admin_product/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    #[Route('/admin/product/new', name: 'admin_product_new')]
+    public function new(ObjectManager $manager, Request $request): Response
+    {
+        $product = new Product;
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+            $manager->persist($product);
+            $manager->flush();
+        
+            $this->addFlash('success','Le produit a bien été ajouté');
+
+            return $this->redirectToRoute('admin_product');
+        }
+
+        dump($product);
+
+        return $this->render('admin_product/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
