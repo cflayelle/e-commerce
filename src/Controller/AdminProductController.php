463ebb47f3759144efb84cff6bb64b562d743825
Mon Macbook <wendykrwn@gmail.com>
2022-03-17 23:15:32 +0100

    gestion produit

---
+++ src/Controller/AdminProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
+
+
+class AdminProductController extends AbstractController
+{
+    #[Route('/admin/product', name: 'app_admin_product')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('admin_product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
