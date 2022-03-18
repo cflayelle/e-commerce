e4942baa82331df306c41ecac41b113ef0b7b383
Mon Macbook <wendykrwn@gmail.com>
2022-03-18 07:43:41 +0100

    ajout categorie

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +19,29 @@ class AdminCategoryController extends AbstractController
         $categories = $categoryRepository->findAll();
         return $this->render('admin_category/index.html.twig', [
             "categories" => $categories
+        ]);
+    }
+
+    #[Route('/admin/category/new', name: 'admin_category_new')]
+    public function new(ObjectManager $manager, Request $request): Response
+    {
+        $category = new Category;
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        
+            $manager->persist($category);
+            $manager->flush();
+        
+            $this->addFlash('success','La catégorie a bien été ajouté');
+
+            return $this->redirectToRoute('admin_category');
+        }
+
+        dump($category);
+
+        return $this->render('admin_category/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
