e8d7ffd80057537dde5856253cd69dc8a88ea6d7
Mon Macbook <wendykrwn@gmail.com>
2022-03-23 18:18:31 +0100

    pouvoir modifier le statut d'une commande

--- src/Controller/AdminPurchaseController.php
+++ src/Controller/AdminPurchaseController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Purchase;
+use App\Form\PurchaseType;
 use App\Repository\PurchaseRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +19,27 @@ class AdminPurchaseController extends AbstractController
         $purchases = $purchaseRepository->findAll();
         return $this->render('admin_purchase/index.html.twig', [
             'purchases' => $purchases,
+        ]);
+    }
+    #[Route('/admin/commande/edit/{id}', name: 'admin_purchase_edit')]
+    public function edit(Purchase $purchase, Request $request, ObjectManager $manager): Response
+    {
+        $form = $this->createForm(PurchaseType::class, $purchase);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($purchase);
+            $manager->flush();
+
+            $this->addFlash('success', 'La commande a bien été modifié');
+
+            return $this->redirectToRoute('admin_purchase');
+        }
+
+        return $this->render('admin_purchase/edit.html.twig', [
+            'form' => $form->createView(),
+            'purchase'=>$purchase
         ]);
     }
 }
