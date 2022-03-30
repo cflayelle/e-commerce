9a862a7fc3df98bfc2e2532730c560e51dcb8e6f
Flayelle Camille <camille@deCamille.local>
2022-03-30 14:10:26 +0200

    affichage de la fiche produit

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -23,7 +23,7 @@ class Product
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'text')]
     private $description;
 
     #[ORM\Column(type: 'integer')]
