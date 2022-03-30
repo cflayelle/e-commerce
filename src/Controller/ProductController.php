7b1b2b1154d88b10535482a2c3484d8fed36a82c
Flayelle Camille <camille@deCamille.local>
2022-03-30 10:53:19 +0200

    Affichage des produits d'une  categorie

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,26 @@ use App\Repository\ProductRepository;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/products", name="product")
+     * @Route("/produits", name="product")
      */
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findProductsAvailable();
 
         return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @Route("/categorie/{id}/produits", name="categorie_products")
+     */
+    public function showProduct(Category $category): Response
+    {
+        $products = $category->getProducts();
+
+        return $this->render('product/showCategorieProducts.html.twig', [
             'products' => $products
         ]);
     }
